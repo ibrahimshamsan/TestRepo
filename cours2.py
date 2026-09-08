0c8a0f5eb59ebf3ccaba80f778cd85a1cb1f18b7
ibrahim <ibrahim7shamsan@gmail.com>
2026-09-08 22:25:50 +0300

    Add files via upload

---
+++ cours2.py
@@ -0,0 +1,22 @@
+# userName=input('name:')
+# password=input('pass:')
+
+# if userName =='ibrahim' and password=='666':
+#     print('welcome')
+# else:
+#     print('error')   
+
+
+# my_list=[1,2,3,4,5,6,7,8,9,10]
+# counter=0
+# for i in my_list:
+#     counter=counter+i
+# print(counter)
+
+# for _ in range(0,10,2):
+#  print(_)
+
+# for i,char in enumerate(list(range(100))):
+#     if char==50:
+#      print(f'index of 50 is: {i}')
+
